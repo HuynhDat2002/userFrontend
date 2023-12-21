a12a284c96d40ba83fddc87ae716f65d51bed899
TronggDaii <trongdai0914@gmail.com>
2023-12-21 20:16:13 +0700

    a

--- src/App.js
+++ src/App.js
@@ -30,6 +30,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="profile" element={<About />} />
+
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
             <Route path="product" element={<OurStore />} />
