a9e8f137a2d2358752d36c5fe757f27205af89de
HaiQuan2002 <xichdenh@gmail.com>
2023-12-23 19:17:44 +0700

    productcard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -21,56 +21,46 @@ const ProductCard = (props) => {
     dispatch(addToWishlist(id));
 
   };
-console.log("w",data)
+  console.log("w", data)
   return (
     <>
-      { data?.map((item, index) => {
-          return (
-            <div
+      {data?.map((item, index) => {
+        return (
+          <div
 
-              key={index}
-              className={` ${
-                location.pathname === "/product" ? `gr-${grid}` : "col-3"
+            key={index}
+            className={` ${location.pathname === "/product" ? `gr-${grid}` : "col-3"
               } `}
+          >
+            <Link
+              // to={`${
+              //   location.pathname == "/"
+              //     ? "/product/:id"
+              //     : location.pathname == "/product/:id"
+              //     ? "/product/:id"
+              //     : ":id"
+
+              //   }`}
+              className="product-card position-relative"
             >
-              <Link
-                // to={`${
-                //   location.pathname == "/"
-                //     ? "/product/:id"
-                //     : location.pathname == "/product/:id"
-                //     ? "/product/:id"
-                //     : ":id"
-
-                //   }`}
-                className="product-card position-relative"
-              >
-                <div className="wishlist-icon position-absolute">
-                  <button
-                    className="border-0 bg-transparent"
-                    onClick={() => {
-                      addToWish(item?._id);
-                    }}
-                  >
-                    <img
-                      src={wish}
-                      alt="wishlist"
-                    />
-
-           
-                  
+              <div className="wishlist-icon position-absolute">
+                <button
+                  className="border-0 bg-transparent"
+                  onClick={() => {
+                    addToWish(item?._id);
+                  }}
+                >
+                  <img
+                    src={wish}
+                    alt="wishlist"
+                  />
                 </button>
               </div>
               <div className="product-image">
-                <img 
-                  //src={item?.images[0].url}
-                  className="img-fluid d-block mx-auto" 
-                  alt="product image" 
-                  width={160}
-                />
-                <img 
-                  src={watch2} 
-                  className="img-fluid mx-auto" 
-                  alt="product image" 
+                <img
+                  src={item?.images[0]?.url}
+                  className="img-fluid d-block mx-auto"
+                  alt="product image"
                   width={160}
                 />
               </div>
@@ -86,11 +76,10 @@ console.log("w",data)
                   edit={false}
                   activeColor="#ffd700"
                 />
-                <p 
-                  className={`description ${
-                    grid === 12 ? "d-block" : "d-none"
+                <p
+                  className={`description ${grid === 12 ? "d-block" : "d-none"
                     }`}
-                    dangerouslySetInnerHTML={{__html: item?.description}}
+                  dangerouslySetInnerHTML={{ __html: item?.description }}
                 ></p>
                 <p className="price">$ {item?.price}</p>
               </div>
@@ -107,51 +96,15 @@ console.log("w",data)
 
                   </button>
                 </div>
-                <div className="product-image">
-                  <img
-                    src={item?.images[0]?.url}
-                    className="img-fluid d-block mx-auto"
-                    alt="product image"
-                    width={160}
-                  />
 
-                </div>
-                <div className="product-details">
-                  <h6 className="brand">{item?.brand}</h6>
-                  <h5 className="product-title" key={index}>
-                    {item?.title}
-                  </h5>
-                  <ReactStars
-                    count={5}
-                    size={24}
-                    value={parseFloat(item?.totalrating)}
-                    edit={false}
-                    activeColor="#ffd700"
-                  />
-                  <p
-                    className={`description ${grid === 12 ? "d-block" : "d-none"
-                      }`}
-                    dangerouslySetInnerHTML={{ __html: item?.description }}
-                  ></p>
-                  <p className="price">$ {item?.price}</p>
-                </div>
-                <div className="action-bar position-absolute">
-                  <div className="d-flex flex-column gap-15">
-                    <button className="border-0 bg-transparent">
-                      <img src={prodcompare} alt="compare" />
-                    </button>
-                    <button className="border-0 bg-transparent">
-                      <img src={view} alt="view" />
-                    </button>
-                    <button className="border-0 bg-transparent">
-                      <img src={addcart} alt="addcart" />
-                    </button>
-                  </div>
-                </div>
-              </Link>
-            </div>
-          )
-        })
+              </div>
+            </Link>
+
+
+          </div>
+        )
+      }
+      )
       }
 
 
