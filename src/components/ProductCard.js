fdb126f94b7357ad285891c863a189542135cc88
TronggDaii <trongdai0914@gmail.com>
2023-12-23 18:57:58 +0700

    div

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -21,56 +21,55 @@ const ProductCard = (props) => {
     dispatch(addToWishlist(id));
 
   };
-console.log("w",data)
+  console.log("w", data)
   return (
     <>
-      { data?.map((item, index) => {
-          return (
-            <div
+      {data?.map((item, index) => {
+        return (
+          <div
 
-              key={index}
-              className={` ${
-                location.pathname === "/product" ? `gr-${grid}` : "col-3"
+            key={index}
+            className={` ${location.pathname === "/product" ? `gr-${grid}` : "col-3"
               } `}
+          >
+            <Link
+              // to={`${
+              //   location.pathname == "/"
+              //     ? "/product/:id"
+              //     : location.pathname == "/product/:id"
+              //     ? "/product/:id"
+              //     : ":id"
+
+              //   }`}
+              className="product-card position-relative"
             >
-              <Link
-                // to={`${
-                //   location.pathname == "/"
-                //     ? "/product/:id"
-                //     : location.pathname == "/product/:id"
-                //     ? "/product/:id"
-                //     : ":id"
+              <div className="wishlist-icon position-absolute">
+                <button
+                  className="border-0 bg-transparent"
+                  onClick={() => {
+                    addToWish(item?._id);
+                  }}
+                >
+                  <img
+                    src={wish}
+                    alt="wishlist"
+                  />
 
-                //   }`}
-                className="product-card position-relative"
-              >
-                <div className="wishlist-icon position-absolute">
-                  <button
-                    className="border-0 bg-transparent"
-                    onClick={() => {
-                      addToWish(item?._id);
-                    }}
-                  >
-                    <img
-                      src={wish}
-                      alt="wishlist"
-                    />
 
-           
-                  
+
                 </button>
               </div>
               <div className="product-image">
-                <img 
+                <img
                   //src={item?.images[0].url}
-                  className="img-fluid d-block mx-auto" 
-                  alt="product image" 
+                  className="img-fluid d-block mx-auto"
+                  alt="product image"
                   width={160}
                 />
-                <img 
-                  src={watch2} 
-                  className="img-fluid mx-auto" 
-                  alt="product image" 
+                <img
+                  src={watch2}
+                  className="img-fluid mx-auto"
+                  alt="product image"
                   width={160}
                 />
               </div>
@@ -86,11 +85,10 @@ console.log("w",data)
                   edit={false}
                   activeColor="#ffd700"
                 />
-                <p 
-                  className={`description ${
-                    grid === 12 ? "d-block" : "d-none"
+                <p
+                  className={`description ${grid === 12 ? "d-block" : "d-none"
                     }`}
-                    dangerouslySetInnerHTML={{__html: item?.description}}
+                  dangerouslySetInnerHTML={{ __html: item?.description }}
                 ></p>
                 <p className="price">$ {item?.price}</p>
               </div>
@@ -148,10 +146,12 @@ console.log("w",data)
                     </button>
                   </div>
                 </div>
-              </Link>
-            </div>
-          )
-        })
+                </div>
+            </Link>
+          </div>
+
+        )
+      })
       }
 
 
